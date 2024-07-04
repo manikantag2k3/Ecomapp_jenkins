352a48c15f8c4010bbb1736e2697dbb002a18765
manikantag2k3 <manikantagandla2@gmail.com>
2024-07-04 12:54:03 +0530

    web-hook testing

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,7 +14,7 @@ exports.createProduct = async (req, res) => {
 exports.getProducts = async (req, res) => {
     try {
         const products = await Product.find();
-        console.log("reached get products");
+        console.log("Products");
         res.status(200).json(products);
     } catch (error) {
         res.status(400).json({ error: error.message });
